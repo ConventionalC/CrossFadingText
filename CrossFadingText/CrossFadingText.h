56686972f3de1cc1c56236919d01fbfbe65ab951
Peter DeWeese <peter.deweese@gmail.com>
2015-02-13 11:55:13 -0500

    Initial Commit

---
+++ CrossFadingText/CrossFadingText.h
@@ -0,0 +1,19 @@
+//
+//  CrossFadingText.h
+//  CrossFadingText
+//
+//  Created by Peter DeWeese on 2/13/15.
+//  Copyright (c) 2015 DeWeese Consulting, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CrossFadingText.
+FOUNDATION_EXPORT double CrossFadingTextVersionNumber;
+
+//! Project version string for CrossFadingText.
+FOUNDATION_EXPORT const unsigned char CrossFadingTextVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CrossFadingText/PublicHeader.h>
+
+
